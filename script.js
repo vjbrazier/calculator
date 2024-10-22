d9063895615d1abd6d7a167628e3fb02a366affb
vjbrazier <vincentbrazier64@gmail.com>
2024-10-22 17:01:27 -0400

    History panel finished
    No history saving.
    Added in some conditionals to account for user error.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Creates two arrays, one for the numbers and one for symbols
 let numbers = document.getElementsByClassName('calc-number');
-let symbols = document.getElementsByClassName('calc-button'); //separated to add spaces
+let symbols = document.getElementsByClassName('calc-button'); //separated to add spaces with symbols
 
 //Stores the equation as the user enters numbers. Starts blank
 var current_equation = '';
@@ -14,31 +14,42 @@ for (var i = 0; i < numbers.length; i++) {
 }
 for (var i = 0; i < symbols.length; i++) {
     symbols[i].addEventListener('click', function(event) {
-        document.getElementById('problem-textbox').innerHTML += (' ' + event.target.innerHTML + ' ');
-        if (event.target.innerHTML != '=') {
-            current_equation += (' ' + event.target.innerHTML + ' ');
+        if ((event.target.innerHTML != '=') && (event.target.innerHTML != 'CE')) {
+            //This if statement ensures you don't start with a symbol, and don't enter one twice
+            if ((current_equation != '') && (current_equation.substring((current_equation.length-1), current_equation.length) != ' ')) { 
+                document.getElementById('problem-textbox').innerHTML += (' ' + event.target.innerHTML + ' ');
+                current_equation += (' ' + event.target.innerHTML + ' ');
+            }    
         }
     });
 }
 
+//Opens/closes the history panel when the button is clicked
+document.getElementById('button-history').addEventListener('click', function() {
+    document.getElementById('history-panel').classList.toggle('visible');
+});
+
+document.getElementById('close-history').addEventListener('click', function() {
+    console.log("close click");
+    document.getElementById('history-panel').classList.toggle('visible');
+});
+
 //Clears the textbox and current equation when clicking CE
-if (document.getElementById('button-CE').addEventListener('click', function(event) {
+document.getElementById('button-CE').addEventListener('click', function(event) {
     document.getElementById('problem-textbox').innerHTML = '';
     document.getElementById('answer-textbox').innerHTML = '';
     current_equation = '';
-}));
+});
 
 /*
 This is for solving the equation. There are several steps.
 It first creates an array that splits the equation based on spaces.
-Sample: ['30', '+', '40']
 
 Then it converts all numbers into integers, rather than a string
-Sample: [30, '+', '40']
 
-Then, it goes through and looks for multiplication and division
-If it finds any, it calculates each part, and updates the array.
-If does this until no more remains
+Then, it goes through and looks for multiplication and division symbols
+If it finds any, it calculates with the index before and after the symbol, and updates the array
+It does this until all the symbols are gone
 
 This process is repeated for addition and subtraction
 
@@ -95,9 +106,19 @@ function solve(equation) {
     return chunks[0];
 }
 
+
+
 //Used to display the answer, update the history, and reset the current equation
-if (document.getElementById('button-equals').addEventListener('click', function(event) {
-    document.getElementById('problem-textbox').innerHTML = '';
-    document.getElementById('answer-textbox').innerHTML = solve(current_equation);
-    current_equation = '';
-}));
+document.getElementById('button-equals').addEventListener('click', function(event) {
+    
+    //Checks that the equation being passed through isn't blank or invalid
+    
+    
+    if ((current_equation != '') && (current_equation.substring((current_equation.length-1), current_equation.length) != ' ')) {
+        document.getElementById('problem-textbox').innerHTML = '';
+        document.getElementById('answer-textbox').innerHTML = solve(current_equation);
+        current_equation = '';
+    } else {
+        document.getElementById('answer-textbox').innerHTML = "It looks like your equation is blank, or unfinished.";
+    }; 
+});
