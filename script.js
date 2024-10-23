009bf059b8b15de037ba9c552ba038ff27749049
Vincent <vincentbrazier64@gmail.com>
2024-10-23 13:36:10 -0400

    History is finished. Background changes colors. Need to tweak responsiveness.

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
+//These variables point to various parts of the page and are here for convenience
+problem = document.getElementById('problem-textbox');
+answer  = document.getElementById('answer-textbox');
+body    = document.getElementById('webpage');
+
 //Creates two arrays, one for the numbers and one for symbols
 let numbers = document.getElementsByClassName('calc-number');
 let symbols = document.getElementsByClassName('calc-button'); //separated to add spaces with symbols
 
-//Stores the equation as the user enters numbers. Starts blank
-let current_equation = '';
-let problem_history = [];
-let answer_history = [];
-let history_total = 0;
+let current_equation = ''; //Stores the equation as the user enters numbers. Starts blank
+let problem_history = [];  //Stores entered problems
+let answer_history = [];   //Stores answers
+let history_total = 0;     //The total number of items in the history
 
-//Adds the numbers/symbols to the problem text. Symbols need a space, hence the separation
+function randomColor() {
+    color1 = Math.floor(Math.random() * 255) + 1;
+    color2 = Math.floor(Math.random() * 255) + 1;
+    color3 = Math.floor(Math.random() * 255) + 1;
+    return 'rgb(' + color1 + ',' + color2 + ',' + color3 + ')'
+}
+
+//Adds the numbers/symbols to the problem text. Symbols need a space, hence the separate loop
 for (var i = 0; i < numbers.length; i++) {
     numbers[i].addEventListener('click', function(event) {
-        document.getElementById('problem-textbox').innerHTML += (event.target.innerHTML);
-        current_equation += (event.target.innerHTML);
+        body.style.backgroundColor = randomColor();
+        problem.innerHTML += (event.target.innerHTML);
+        current_equation  += (event.target.innerHTML);
     });
 }
 for (var i = 0; i < symbols.length; i++) {
@@ -20,7 +32,8 @@ for (var i = 0; i < symbols.length; i++) {
         if ((event.target.innerHTML != '=') && (event.target.innerHTML != 'CE')) {
             //This if statement ensures you don't start with a symbol, and don't enter one twice
             if ((current_equation != '') && (current_equation.substring((current_equation.length-1), current_equation.length) != ' ')) { 
-                document.getElementById('problem-textbox').innerHTML += (' ' + event.target.innerHTML + ' ');
+                body.style.backgroundColor = randomColor();
+                problem.innerHTML += (' ' + event.target.innerHTML + ' ');
                 current_equation += (' ' + event.target.innerHTML + ' ');
             }    
         }
@@ -31,6 +44,7 @@ for (var i = 0; i < symbols.length; i++) {
 //This was working, but after adding the history functionality, it stopped.
 //The conditional that makes each one unique came from ChatGPT.
 document.getElementById('button-history').addEventListener('click', function() {
+    body.style.backgroundColor = randomColor();
     document.getElementById('history-panel').classList.toggle('visible');
     
     // if (!document.getElementById('history-panel').classList.contains('visible')) {
@@ -45,8 +59,9 @@ document.getElementById('button-history').addEventListener('click', function() {
 
 //Clears the textbox and current equation when clicking CE
 document.getElementById('button-CE').addEventListener('click', function(event) {
-    document.getElementById('problem-textbox').innerHTML = '';
-    document.getElementById('answer-textbox').innerHTML = '';
+    body.style.backgroundColor = randomColor();
+    problem.innerHTML = '';
+    answer.innerHTML = '';
     current_equation = '';
 });
 
@@ -117,45 +132,32 @@ function solve(equation) {
 */
 function addToHistory(equation) {
     problem_history[history_total] = current_equation;
-    answer_history[history_total] = solve(current_equation);
-    document.getElementById('history-list').innerHTML += '<p id="p' + history_total + '">' + problem_history[history_total] + '</p>' +
-                                                         '<p id="a' + history_total + '">= ' + answer_history[history_total] + '</p>' +
-                                                         '<button id="lp' + history_total + '" class="load-problem" >Load Problem ' + history_total + '</button>' +
-                                                         '<button id="la' + history_total + '" class="load-answer"  >Load Answer ' + history_total + '</button>' +
-                                                         '<button id="d' + history_total + '" class="delete-entry" >Delete Entry ' + history_total + '</button>';
+    answer_history[history_total]  = solve(current_equation);
+    document.getElementById('history-list').innerHTML += '<div id="entry-' + history_total + '">' +
+                                                         '<p>'       + problem_history[history_total] + '</p>' +
+                                                         '<p>= '     + answer_history[history_total]  + '</p>' +
+                                                         '<button class="load-problem">Load Problem ' + history_total + '</button>' +
+                                                         '<button class="load-answer" >Load Answer '  + history_total + '</button>' +
+                                                         '<button id="delete-' + history_total + '" class="delete-entry">Delete Entry ' + history_total + '</button>' +
+                                                         '</div>';
     history_total ++;
 }
 
-//Used to display the answer, update the history, and reset the current equation
-document.getElementById('button-equals').addEventListener('click', function(event) {
-    
-    //Checks that the equation being passed through isn't blank or invalid
-    if ((current_equation != '') && (current_equation.substring((current_equation.length-1), current_equation.length) != ' ')) {
-        addToHistory(current_equation);
-        document.getElementById('problem-textbox').innerHTML = '';
-        document.getElementById('answer-textbox').innerHTML = solve(current_equation);
-        update_history();
-        current_equation = '';
-    } else {
-        document.getElementById('answer-textbox').innerHTML = "It looks like your equation is blank, or unfinished.";
-    }; 
-});
-
 function update_history() {
-    let load_problems = document.getElementsByClassName('load-problem');
-    let load_answers = document.getElementsByClassName('load-answer');
+    let load_problems  = document.getElementsByClassName('load-problem');
+    let load_answers   = document.getElementsByClassName('load-answer');
     let delete_entries = document.getElementsByClassName('delete-entry');
 
     for (let i = 0; i < load_problems.length; i++) {
         load_problems[i].addEventListener('click', function(event) {
             if (history_total < 10) {
                 if (i == (event.target.innerHTML.substring(13,14))) {
-                    document.getElementById('problem-textbox').innerHTML += problem_history[i];
+                    problem.innerHTML += problem_history[i];
                     current_equation += problem_history[i];
                 } 
             } else {
                 if (i == (event.target.innerHTML.substring(13,15))) {
-                    document.getElementById('problem-textbox').innerHTML += problem_history[i];
+                    problem.innerHTML += problem_history[i];
                     current_equation += problem_history[i];
                 } ;
             };
@@ -166,42 +168,51 @@ function update_history() {
         load_answers[i].addEventListener('click', function(event) {
             if (history_total < 10) {
                 if (i == (event.target.innerHTML.substring(12,13))) {
-                    document.getElementById('problem-textbox').innerHTML += answer_history[i];
+                    problem.innerHTML += answer_history[i];
                     current_equation += answer_history[i];
                 }
             } else {
                 if (i == (event.target.innerHTML.substring(12,14))) {
-                    document.getElementById('problem-textbox').innerHTML += answer_history[i];
+                    problem.innerHTML += answer_history[i];
                     current_equation += answer_history[i];
                 };
             };
         });
     };
 
-    for (let i = 0; i < delete_entries.length; i++) {
+    for (let i = 0; i < history_total; i++) {
         delete_entries[i].addEventListener('click', function(event) {
             if (history_total < 10) {
                 if (i == (event.target.innerHTML.substring(13,14))) {
-                    document.getElementById('p' + i).remove();
-                    document.getElementById('a' + i).remove();
-                    load_problems[i].remove();
-                    load_answers[i].remove();
                     problem_history[i] = '';
                     answer_history[i] = '';
-                    delete_entries[i].remove();
+                    document.getElementById('entry-' + i).remove();
+                    history_total--;
                 }
             } else {
                 if (i == (event.target.innerHTML.substring(13,15))) {
-                    document.getElementById('p' + i).remove();
-                    document.getElementById('a' + i).remove();
-                    document.getElementById('lp' + i).remove();
-                    document.getElementById('la' + i).remove();
                     problem_history[i] = '';
                     answer_history[i] = '';
-                    event.target.remove();
-                    //document.getElementById('d' + i).remove();
+                    document.getElementById('entry-' + i).remove();
+                    history_total--;
                 };
             };
         });
     };
 }
+
+//Used to display the answer, update the history, and reset the current equation
+document.getElementById('button-equals').addEventListener('click', function(event) {
+    //Checks that the equation being passed through isn't blank or invalid
+    if ((current_equation != '') && (current_equation.substring((current_equation.length-1), current_equation.length) != ' ')) {
+        body.style.backgroundColor = randomColor();
+        addToHistory(current_equation);
+        problem.innerHTML = '';
+        answer.innerHTML  = solve(current_equation);
+        update_history();
+        current_equation = '';
+    } else {
+        answer.innerHTML = "It looks like your equation is blank, or unfinished.";
+    }; 
+});
+
