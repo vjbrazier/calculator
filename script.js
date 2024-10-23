390bbd811562f12dbf2b30720f10dba0f2e23d32
vjbrazier <vincentbrazier64@gmail.com>
2024-10-22 23:37:36 -0400

    Solving a problem now adds it to the history
    Need to add loading and deleting functionality still.

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ let numbers = document.getElementsByClassName('calc-number');
 let symbols = document.getElementsByClassName('calc-button'); //separated to add spaces with symbols
 
 //Stores the equation as the user enters numbers. Starts blank
-var current_equation = '';
+let current_equation = '';
+let problem_history = [];
+let answer_history = [];
+let history_total = 0;
+
+let load_problems = document.getElementsByClassName('load-problem');
+let load_answers = document.getElementsByClassName('load-answer');
+let delete_entries = document.getElementsByClassName('delete-entry');
 
 //Adds the numbers/symbols to the problem text. Symbols need a space, hence the separation
 for (var i = 0; i < numbers.length; i++) {
@@ -25,13 +32,17 @@ for (var i = 0; i < symbols.length; i++) {
 }
 
 //Opens/closes the history panel when the button is clicked
+//This was working, but after adding the history functionality, it stopped.
+//The conditional that makes each one unique came from ChatGPT.
 document.getElementById('button-history').addEventListener('click', function() {
-    document.getElementById('history-panel').classList.toggle('visible');
+    if (!document.getElementById('history-panel').classList.contains('visible')) {
+        document.getElementById('history-panel').classList.add('visible');
+    }
 });
-
 document.getElementById('close-history').addEventListener('click', function() {
-    console.log("close click");
-    document.getElementById('history-panel').classList.toggle('visible');
+    if (document.getElementById('history-panel').classList.contains('visible')) {
+        document.getElementById('history-panel').classList.remove('visible');
+    }
 });
 
 //Clears the textbox and current equation when clicking CE
@@ -56,65 +67,84 @@ This process is repeated for addition and subtraction
 The final number remaining in the array is then returned as the answer.
 */
 function solve(equation) {
-    let chunks = equation.split(' ');
+    let equation_fragments = equation.split(' ');
     
     //Conversion to integers
-    for (var i = 0; i < chunks.length; i++) {
-        if ((chunks[i] != '+') && (chunks[i] != '-') && 
-            (chunks[i] != '*') && (chunks[i] != '/') ) {
-                chunks[i] = Number(chunks[i]);
+    for (let i = 0; i < equation_fragments.length; i++) {
+        if ((equation_fragments[i] != '+') && (equation_fragments[i] != '-') && 
+            (equation_fragments[i] != '*') && (equation_fragments[i] != '/') ) {
+                equation_fragments[i] = Number(equation_fragments[i]);
         }
     }
-    console.log(chunks);
+    console.log(equation_fragments);
 
     //Multiplication/division
-    while ((chunks.includes('*')) || (chunks.includes('/'))) {
-        for (let i = 0; i < chunks.length; i++) {
-            if (chunks[i] === '*') {
-                chunks[i-1] = chunks[i-1] * chunks[i+1];
-                chunks.splice(i,2);
+    while ((equation_fragments.includes('*')) || (equation_fragments.includes('/'))) {
+        for (let i = 0; i < equation_fragments.length; i++) {
+            if (equation_fragments[i] === '*') {
+                equation_fragments[i-1] = equation_fragments[i-1] * equation_fragments[i+1];
+                equation_fragments.splice(i,2);
                 break;
            }
 
-           if (chunks[i] === '/') {
-                chunks[i-1] = chunks[i-1] / chunks[i+1];
-                chunks.splice(i,2);
+           if (equation_fragments[i] === '/') {
+                equation_fragments[i-1] = equation_fragments[i-1] / equation_fragments[i+1];
+                equation_fragments.splice(i,2);
                 break;
            }
        }
     }
-    console.log(chunks);
+    console.log(equation_fragments);
 
     //Addition/subtraction
-    while ((chunks.includes('+')) || (chunks.includes('/'))) {
-        for (let i = 0; i < chunks.length; i++) {
-            if (chunks[i] === '+') {
-                chunks[i-1] = chunks[i-1] + chunks[i+1];
-                chunks.splice(i,2);
+    while ((equation_fragments.includes('+')) || (equation_fragments.includes('/'))) {
+        for (let i = 0; i < equation_fragments.length; i++) {
+            if (equation_fragments[i] === '+') {
+                equation_fragments[i-1] = equation_fragments[i-1] + equation_fragments[i+1];
+                equation_fragments.splice(i,2);
                 break;
             }
 
-            if (chunks[i] === '-') {
-                chunks[i-1] = chunks[i-1] - chunks[i+1];
-                chunks.splice(i,2);
+            if (equation_fragments[i] === '-') {
+                equation_fragments[i-1] = equation_fragments[i-1] - equation_fragments[i+1];
+                equation_fragments.splice(i,2);
                 break;
             }
         }
     }
-    console.log(chunks);
+    console.log(equation_fragments);
     
-    return chunks[0];
+    return equation_fragments[0];
 }
 
+/*
 
+*/
+function addToHistory(equation) {
+    problem_history[history_total] = current_equation;
+    answer_history[history_total] = solve(current_equation);
+    document.getElementById('history-list').innerHTML += '<p>' + problem_history[history_total] + '</p>' +
+                                                         '<p>= ' + answer_history[history_total] + '</p>' +
+                                                         '<button class="load-problem" id="load-problem-' + history_total + '">Load Problem</button>' +
+                                                         '<button class="load-answer"  id="load-answer-' + history_total + '">Load Answer</button>' +
+                                                         '<button class="delete-entry" id="delete-entry-' + history_total + '">Delete</button>';
+    history_total ++;
+}
+
+for (let i = 0; i < load_problems.length; i++) {
+    load_problems[i].addEventListener('click', function(event) {
+        if (i == event.target.id.substring(event.target.id.length-1, event.target.length)) {
+            
+        } 
+    });
+};
 
 //Used to display the answer, update the history, and reset the current equation
 document.getElementById('button-equals').addEventListener('click', function(event) {
     
     //Checks that the equation being passed through isn't blank or invalid
-    
-    
     if ((current_equation != '') && (current_equation.substring((current_equation.length-1), current_equation.length) != ' ')) {
+        addToHistory(current_equation);
         document.getElementById('problem-textbox').innerHTML = '';
         document.getElementById('answer-textbox').innerHTML = solve(current_equation);
         current_equation = '';
